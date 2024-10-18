fc9b2a1e1e458aae872f99c29fe9f37c5760ff8c
zavocc <zavocc1030@outlook.com>
2024-10-18 16:01:29 +0800

    Avatar command is near complete

--- cogs/ai/gemini/fun.py
+++ cogs/ai/gemini/fun.py
@@ -5,58 +5,26 @@ import discord
 import importlib
 import logging
 
-class Fun(commands.Cog):
-    """Use my fun and trivial utilities here that can help make your server more active and entertaining"""
+class GeminiUtils(commands.Cog):
+    """Gemini powered utilities"""
     def __init__(self, bot):
         self.bot = bot
         self.author = environ.get("BOT_NAME", "Jakey Bot")
 
+        
     @commands.slash_command(
         contexts={discord.InteractionContextType.guild},
         integration_types={discord.IntegrationType.guild_install}
     )
-    async def mimic(self, ctx, user: Member, message_body: str):
-        """Mimic as user!"""
-        await ctx.response.defer(ephemeral=True)
-
-        if isinstance(user, int):
-            user = await self.bot.fetch_user(user)    
-        avatar_url = user.avatar.url if user.avatar else "https://cdn.discordapp.com/embed/avatars/0.png"
-
-        # Set display name depending on whether if the user joins in particular guild or in DMs to have different display names
-        if ctx.guild:
-            _xuser_display_name = await ctx.guild.fetch_member(user.id)
-            user_name = f"{_xuser_display_name.display_name}"
-        else:
-            _xuser_display_name = await self.bot.fetch_user(user.id)
-            user_name = f"{_xuser_display_name.display_name}"
-
-        webhook = await ctx.channel.create_webhook(name=f"Mimic command by {self.author}")
-
-        if not message_body:
-            await ctx.respond("⚠️ Please specify a message to mimic!")
-            return
-        await webhook.send(content=message_body, username=user_name, avatar_url=avatar_url)
-        await webhook.delete()
-        
-        await ctx.respond("✅ Done!")
-
-    @mimic.error
-    async def on_command_error(self, ctx: commands.Context, error: DiscordException):
-        if isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadUnionArgument):
-            await ctx.respond("⚠️ Please specify a valid discord user (or user id) and message to mimic!\n**Syntax:** `$mimic <user/user id> <message>`")
-        elif isinstance(error, commands.CommandInvokeError) or isinstance(error, commands.MissingPermissions):
-            await ctx.respond("❌ Sorry, webhooks are not enabled in this channel. Please enable webhooks in this channel to use this command.")
-        elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.respond("❌ Sorry, this feature is not supported in DMs. Please use this command inside the guild.")
-        elif isinstance(error, commands.ApplicationCommandInvokeError):
-            await ctx.respond("⚠️ Please input a member")
-        else:
-            raise error
-        
-    @commands.slash_command(
-        contexts={discord.InteractionContextType.guild},
-        integration_types={discord.IntegrationType.guild_install}
+    @discord.option(
+        "user",
+        description="A user to get avatar from",
+        required=False
+    )
+    @discord.option(
+        "describe",
+        description="Describe the avatar using Gemini 1.5 Flash",
+        required=False
     )
     async def avatar(self, ctx, user: Member = None, describe: bool = False):
         """Get user avatar"""
@@ -108,5 +76,10 @@ class Fun(commands.Cog):
         if _description: embed.set_footer(text="Using Gemini 1.5 Flash to generate descriptions, result may not be accurate")
         await ctx.respond(embed=embed, ephemeral=True)
 
+    @avatar.error
+    async def on_command_error(self, ctx: commands.Context, error: DiscordException):
+        await ctx.respond("⛔ Something went wrong, please check console log for details")
+        raise error
+
 def setup(bot):
-    bot.add_cog(Fun(bot))
+    bot.add_cog(GeminiUtils(bot))
---
+++ cogs/misc.py
@@ -0,0 +1,56 @@
+from discord.ext import commands
+from discord import Member, DiscordException
+from os import environ
+import discord
+
+class Misc(commands.Cog):
+    """Use my other utilities here that can help make your server more active and entertaining"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.author = environ.get("BOT_NAME", "Jakey Bot")
+
+    @commands.slash_command(
+        contexts={discord.InteractionContextType.guild},
+        integration_types={discord.IntegrationType.guild_install}
+    )
+    async def mimic(self, ctx, user: Member, message_body: str):
+        """Mimic as user!"""
+        await ctx.response.defer(ephemeral=True)
+
+        if isinstance(user, int):
+            user = await self.bot.fetch_user(user)    
+        avatar_url = user.avatar.url if user.avatar else "https://cdn.discordapp.com/embed/avatars/0.png"
+
+        # Set display name depending on whether if the user joins in particular guild or in DMs to have different display names
+        if ctx.guild:
+            _xuser_display_name = await ctx.guild.fetch_member(user.id)
+            user_name = f"{_xuser_display_name.display_name}"
+        else:
+            _xuser_display_name = await self.bot.fetch_user(user.id)
+            user_name = f"{_xuser_display_name.display_name}"
+
+        webhook = await ctx.channel.create_webhook(name=f"Mimic command by {self.author}")
+
+        if not message_body:
+            await ctx.respond("⚠️ Please specify a message to mimic!")
+            return
+        await webhook.send(content=message_body, username=user_name, avatar_url=avatar_url)
+        await webhook.delete()
+        
+        await ctx.respond("✅ Done!")
+
+    @mimic.error
+    async def on_command_error(self, ctx: commands.Context, error: DiscordException):
+        if isinstance(error, commands.MissingRequiredArgument) or isinstance(error, commands.BadUnionArgument):
+            await ctx.respond("⚠️ Please specify a valid discord user (or user id) and message to mimic!\n**Syntax:** `$mimic <user/user id> <message>`")
+        elif isinstance(error, commands.CommandInvokeError) or isinstance(error, commands.MissingPermissions):
+            await ctx.respond("❌ Sorry, webhooks are not enabled in this channel. Please enable webhooks in this channel to use this command.")
+        elif isinstance(error, commands.NoPrivateMessage):
+            await ctx.respond("❌ Sorry, this feature is not supported in DMs. Please use this command inside the guild.")
+        elif isinstance(error, commands.ApplicationCommandInvokeError):
+            await ctx.respond("⚠️ Please input a member")
+        else:
+            raise error
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
