dae53f89882c2a817a1b349c78a2dc212995a3b7
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-11-26 11:39:42 +0000

    File upload
    DO NOT USE: I KEEP GETTING 400 ERRORS
    USE THIS AS CHECKPOINT

--- core/ai/models/gemini_rest/infer.py
+++ core/ai/models/gemini_rest/infer.py
@@ -1,5 +1,10 @@
 from os import environ
+from pathlib import Path
 import aiohttp
+import aiofiles
+import discord
+import json
+import random
 
 class Completions():
     _model_provider_thread = "gemini_rest"
@@ -15,7 +20,9 @@ class Completions():
         self._guild_id = guild_id
         self._history_management = db_conn
 
+        # REST parameters
         self._api_endpoint = "https://generativelanguage.googleapis.com/v1beta"
+        self._api_endpoint_upload = "https://generativelanguage.googleapis.com/upload/v1beta/files"
         self._headers = {"Content-Type": "application/json"}
         self._generation_config = {
             "temperature": 1,
@@ -24,6 +31,72 @@ class Completions():
             "maxOutputTokens": 8192,
             "responseMimeType": "text/plain"
         }
+
+    # Chunker for file uploads using aiofiles and this is a generator method
+    async def _chunker(self, file_path, chunk_size=8192):
+        async with aiofiles.open(file_path, "rb") as _file:
+            while True:
+                _chunk = _file.read(chunk_size)
+                if not _chunk:
+                    break
+                yield _chunk
+
+
+    async def input_files(self, attachment: discord.Attachment):
+        _xfilename = f"{environ.get('TEMP_DIR')}/JAKEY.{random.randint(518301839, 6582482111)}.{attachment.filename}"
+        try:
+            async with aiohttp.ClientSession() as _download_session:
+                async with _download_session.get(attachment.url, allow_redirects=True) as _xattachments:
+                    # write to file with random number ID
+                    async with aiofiles.open(_xfilename, "wb") as filepath:
+                        async for _chunk in _xattachments.content.iter_chunked(8192):
+                            await filepath.write(_chunk)
+        except aiohttp.ClientError as httperror:
+            # Remove the file if it exists ensuring no data persists even on failure
+            if Path(_xfilename).exists():
+                await aiofiles.os.remove(_xfilename)
+            # Raise exception
+            raise httperror
+        
+        # Upload the file to the GCS ephemeral bucket
+        _initial_headers = {
+            "X-Goog-Upload-Protocol": "resumable",
+            "X-Goog-Upload-Command": "start",
+            "X-Goog-Upload-Headers-Content-Length": str(attachment.size),
+            "X-Goog-Upload-Headers-Content-Type": attachment.content_type,
+            "Content-Type": "application/json"
+        }
+        _upload_payload = {
+            "file": {
+                "display_name": _xfilename.split("/")[-1]
+            }
+        }
+
+        # Upload the file
+        _upload_status = None
+        async with aiohttp.ClientSession() as _upload_session:
+            # Initial resumable request so we can put the binary data later
+            async with _upload_session.post(f"{self._api_endpoint_upload}?key={environ.get('GEMINI_API_KEY')}",
+                                            headers=_initial_headers,
+                                            json=_upload_payload) as _upload_response:
+                # Raise an error if the request was not successful
+                if _upload_response.status != 200:
+                    raise Exception(f"Upload failed with status code {_upload_response.status}, with reason {_upload_response.reason}")
+                
+                # Get the upload status
+                _upload_status = await _upload_response.content.read()
+                print(_upload_status)
+            
+            async with _upload_session.post(f"{self._api_endpoint_upload}?key={environ.get('GEMINI_API_KEY')}",
+                                            ) as _upload_response:
+                # Raise an error if the request was not successful
+                if _upload_response.status != 200:
+                    raise Exception(f"Upload failed with status code {_upload_response.status}, with reason {_upload_response.reason}")
+
+                _upload_status = await _upload_response.json()
+                print(_upload_status)
+
+        self._file_data = {"url":_upload_status["file"]["uri"], "mime_type":attachment.content_type}
 
     async def chat_completion(self, prompt, system_instruction: str = None):
         # Load history
@@ -55,6 +128,22 @@ class Completions():
                     "role": "user",
                 }
             )
+            
+        # Check if file attachment is present
+        if self._file_data is not None:
+            _chat_thread.append(
+                {
+                    "parts": [
+                        {
+                            "fileData": {
+                                "fileUri": self._file_data["url"],
+                                "mimeType": self._file_data["mime_type"]
+                            }
+                        }
+                    ],
+                    "role": "user"
+                }
+            )
 
         # Payload
         _payload = {
@@ -78,7 +167,7 @@ class Completions():
                                     json=_payload) as response:
                 # Raise an error if the request was not successful
                 if response.status != 200:
-                    raise Exception(f"Request failed with status code {response.status}")
+                    raise Exception(f"Request failed with status code {response.status} with reason {response.reason}")
 
                 _response = await response.json()
                 await self._discord_method_send(_response)
