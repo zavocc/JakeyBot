2c0650117ec9b27c85b64ff9b5750a110dc542a0
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-11-26 03:02:42 +0000

    Add Gemini REST version
    Avoiding the Gemini Python SDK in the long run due to variety of issues such as function calling, monitoring, and the use of pickles
    
    Works:
    - Response Generation
    - Tools (Code Execution)
    
    WIP:
    - Multimodality
    - Tools (all tools)
    - History
    - Thread saving

---
+++ core/ai/models/gemini_rest/infer.py
@@ -0,0 +1,82 @@
+from os import environ
+import aiohttp
+
+class Completions():
+    _model_provider_thread = "gemini_rest"
+
+    def __init__(self, guild_id = None, 
+                 model_name = "gemini-1.5-flash-002",
+                 db_conn = None):
+        super().__init__()
+
+        self._file_data = None
+
+        self._model_name = model_name
+        self._guild_id = guild_id
+        self._history_management = db_conn
+
+        self._api_endpoint = "https://generativelanguage.googleapis.com/v1beta"
+
+    async def chat_completion(self, prompt, system_instruction: str = None):
+        # Headers
+        _headers = {
+            "Content-Type": "application/json"
+        }
+    
+        # Request payload
+        _payload = {
+            "systemInstruction": {
+                "role": "user",
+                "parts": [
+                    {
+                        "text": system_instruction
+                    }
+                ]
+            }
+        }
+
+        # Tools
+        _payload.update({
+            "tools": [
+                {"code_execution": {}}
+            ]
+        })
+
+        # User prompt
+        _payload.update({
+            "contents": [
+                {
+                    "role": "user",
+                    "parts": [
+                        {
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        })
+
+        # Generation config
+        _payload.update({
+            "generationConfig": {
+                "temperature": 1,
+                "topK": 40,
+                "topP": 0.95,
+                "maxOutputTokens": 8192,
+                "responseMimeType": "text/plain"
+            }
+        })
+
+        # POST request
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{self._api_endpoint}/models/{self._model_name}:generateContent?key={environ.get('GEMINI_API_KEY')}",
+                                    headers=_headers,
+                                    json=_payload) as response:
+                _response = await response.json()
+
+                await self._discord_method_send(_response)
+
+        return {"answer": _response["candidates"][0]["content"]["parts"][-1]["text"], "chat_thread": None}
+
+    async def save_to_history(self, chat_thread = None):
+        raise Exception("This version is for testing purposes only, function is not implemented")
