42a45c399b3dce88fd71103a82fa581d089ff4db
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-12-01 09:17:58 +0000

    YouTube: use YouTube Data v3 API instead of piped

--- tools/youtube.py
+++ tools/youtube.py
@@ -1,4 +1,5 @@
 # Built in Tools
+from os import environ
 import aiohttp
 
 # Function implementations
@@ -18,36 +19,88 @@ class Tool:
                     "parameters": {
                         "type": "object",
                         "properties": {
-                            "videoid": {
+                            "query": {
                                 "type": "string"
-                            }
+                            },
+                            "n_results": {
+                                "type": "integer",
+                            },
                         },
-                        "required": ["videoid"]
+                        "required": ["query"]
                     }
                 }
             ]
         }
 
     
-    async def _tool_function(self, videoid: str):
+    async def _tool_function(self, query: str, n_results: int = 10):
+        # Must not be above 50
+        if n_results > 50:
+            n_results = 10
+
         # Using piped.video to get the video data
         if not hasattr(self.discord_bot, "_aiohttp_main_client_session"):
             raise Exception("aiohttp client session for get requests not initialized, please check the bot configuration")
+        
+        # Check if we have YOUTUBE_DATA_v3_API_KEY is set
+        if not environ.get("YOUTUBE_DATA_v3_API_KEY"):
+            raise ValueError("YouTube Data v3 API key not set, please go to https://console.cloud.google.com/apis/library/youtube.googleapis.com and get an API key under Credentials in API & Services")
 
         _session: aiohttp.ClientSession = self.discord_bot._aiohttp_main_client_session
 
-        async with _session.get(f"https://pipedapi.kavin.rocks/streams/{videoid}") as response:
-            _json_data = await response.json()
+        # YouTube Data v3 API Endpoint
+        _endpoint = "https://www.googleapis.com/youtube/v3/search"
 
-            # Parse JSON into dict
-            return {
-                "title": _json_data['title'],
-                "description": _json_data['description'],
-                "uploader": _json_data['uploader'],
-                "uploader_url": _json_data['uploaderUrl'],
-                "likes": _json_data['likes'],
-                "dislikes": _json_data['dislikes'],
-                "views": _json_data['views']
+        # Parameters
+        _params = {
+            "part": "snippet",
+            "maxResults": n_results,
+            "q": query,
+            "safeSearch": "strict",
+            "key": environ.get("YOUTUBE_DATA_v3_API_KEY")
+        }
+
+        async with _session.get(_endpoint, params=_params) as _response:
+            _data = await _response.json()
+
+            # If the Content-Type is not application/json
+            if "application/json" not in _response.headers["Content-Type"]:
+                raise Exception("The response from the YouTube API is not in JSON format")
+            
+            # If the response is not successful
+            if _response.status != 200:
+                raise Exception(f"Failed to fetch YouTube search results with code {_response.status}, reason: {_response.reason}")
+            
+        # Iterate over items list
+        _videos = [
+            {
+                "guidelines": "You must format the links with [Video Title](Video URL), always provide video links",
+                "rankingByRelevanceGuidelines": "Depending on the user query, rank the videos by relevance based on title, description, and its channel. Including the published date.",
+                "contentGuidelines": "Avoid presenting videos that may be potentially disturbing (e.g. extreme creepypasta, scary PSA horrids, signal intrusion, porn, etc.)",
+                "rules": "If possible, provide a single relevant video link depending on the user query instead of bulleted multiple links",
+                "videos": []
             }
-        
+        ]
+        for _item in _data["items"]:
+            # Check if kind is video
+            if _item["id"]["kind"] != "youtube#video":
+                continue
+
+            _videos[0]["videos"].append({
+                "title": _item["snippet"]["title"],
+                "description": _item["snippet"]["description"],
+                "url": f"https://www.youtube.com/watch?v={_item['id']['videoId']}",
+                "channel": _item["snippet"]["channelTitle"],
+                "publishedAt": _item["snippet"]["publishedAt"]
+            })
+
+        # If the videos list is empty
+        if not _videos[0]["videos"]:
+            return "No videos found for the given query"
+
+        await self.method_send(f"🔍 Searched for **{query}**")
+
+        return _videos
+
+            
             
