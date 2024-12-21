12ed41b404485fbacb6d4f001fe663f4c923aa8c
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-12-21 14:21:15 +0000

    Only block severe outputs

--- core/aimodels/gemini/infer.py
+++ core/aimodels/gemini/infer.py
@@ -24,19 +24,19 @@ class APIParams:
             "safety_settings": [
                 {
                     "category": "HARM_CATEGORY_HARASSMENT",
-                    "threshold": "BLOCK_LOW_AND_ABOVE"
+                    "threshold": "BLOCK_ONLY_HIGH"
                 },
                 {
                     "category": "HARM_CATEGORY_HATE_SPEECH", 
-                    "threshold": "BLOCK_LOW_AND_ABOVE"
+                    "threshold": "BLOCK_ONLY_HIGH"
                 },
                 {
                     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                    "threshold": "BLOCK_LOW_AND_ABOVE"
+                    "threshold": "BLOCK_ONLY_HIGH"
                 },
                 {
                     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                    "threshold": "BLOCK_LOW_AND_ABOVE"
+                    "threshold": "BLOCK_ONLY_HIGH"
                 }
             ]
         }
