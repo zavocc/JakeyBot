811c18af40978edb8b2390a66fc09714580e2f10
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2024-12-23 07:05:08 +0000

    update comment 2

--- core/aimodels/gemini/infer.py
+++ core/aimodels/gemini/infer.py
@@ -238,7 +238,7 @@ class Completions(APIParams):
             await self._discord_method_send(f"> ℹ️ Below is Gemini's 2.0 thinking process and can produce undesirable outputs. Keep in mind that this model doesn't support tools, has 32k context, and only supports image and text inputs.")
             await self._discord_method_send(f"> {_candidateContentResponse.parts[0].text.replace('\n', '\n> ')[:2000]}")
 
-        # Check if tools are used
+        # Iterate through the parts and perform tasks
         _toolInvoke = []
         for _part in _candidateContentResponse.parts:
             if _part.function_call:
