f2ac562ee2374916720bfff4e8efc654a4bd2f0c
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2025-04-06 09:39:12 +0000

    TTS Tool: Use PlayHT voices instead of Azure

--- tools/audio_generator.py
+++ tools/audio_generator.py
@@ -1,10 +1,8 @@
 from os import environ
 import aiohttp
 import discord
-import inspect
 import io
 
-# Function implementations
 class Tool:
     tool_human_name = "Text to Speech"
     def __init__(self, method_send, discord_ctx, discord_bot):
@@ -15,7 +13,7 @@ class Tool:
         self.tool_schema = [
             {
                 "name": "audio_generator",
-                "description": "Generate audio from text using Azure Text-to-Speech",
+                "description": "Generate audio from text",
                 "parameters": {
                     "type": "OBJECT",
                     "properties": {
@@ -25,73 +23,55 @@ class Tool:
                         "voice": {
                             "type": "STRING",
                             "enum": [
-                                "MALE",
-                                "FEMALE",
-                                "CUTE"
+                                "Aaliyah", "Adelaide", "Angelo", "Arista", "Atlas", "Basil", "Briggs", "Calum", "Celeste",
+                                "Cheyenne", "Chip", "Cillian", "Deedee", "Eleanor", "Fritz", "Gail", "Indigo", "Jennifer",
+                                "Judy", "Mamaw", "Mason", "Mikail", "Mitch", "Nia", "Quinn", "Ruby", "Thunder"
                             ]
                         }
                     },
-                    "required": ["text", "voice"]
+                    "required": ["text"]
                 }
             }
         ]
 
-    # Audio generator using Azure Text-to-Speech
-    async def _tool_function(self, text: str, voice: str):
-        # Check if AZURE_TTS_KEY is set
-        if not environ.get("AZURE_TTS_KEY"):
-            raise ValueError("Azure TTS API key is not set, please set it in the environment variables")
+    # Audio generator using PlayHT Groq Voices
+    async def _tool_function(self, text: str, voice: str = "Atlas"):
+        # Check if GROQ_API_KEY is set
+        if not environ.get("GROQ_API_KEY"):
+            raise ValueError("GROQ_API_KEY is not set, please set it in the environment variables")
 
         # Check if global aiohttp client session is initialized
         if not hasattr(self.discord_bot, "_aiohttp_main_client_session"):
             raise Exception("aiohttp client session for get requests not initialized, please check the bot configuration")
         
         _client_session: aiohttp.ClientSession = self.discord_bot._aiohttp_main_client_session
-        
-        # Check if AZURE_TTS_REGION is set if not, assume its eastus
-        if not environ.get("AZURE_TTS_REGION"):
-            environ["AZURE_TTS_REGION"] = "eastus"
-
         _headers = {
-            "Ocp-Apim-Subscription-Key": environ.get("AZURE_TTS_KEY"),
-            "Content-Type": "application/ssml+xml",
-            "X-Microsoft-OutputFormat": "audio-24khz-160kbitrate-mono-mp3"
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {environ.get('GROQ_API_KEY')}"
         }
+        _payload = {
+            "model": "playai-tts",
+            "voice": f"{voice}-PlayAI",
+            "input": text,
+            "response_format": "wav"
+        }
+        # Endpoint
+        _endpoint = "https://api.groq.com/openai/v1/audio/speech"
 
-        # Check for voices
-        # MALE = en-US-DerekMultilingualNeural
-        # FEMAILE = en-US-JennyNeural
-        # CUTE = en-US-AnaNeural
-        if voice == "MALE":
-            _voiceType = "en-US-DerekMultilingualNeural"
-            _voiceGender = "Male"
-        elif voice == "FEMALE":
-            _voiceType = "en-US-JennyNeural"
-            _voiceGender = "Female"
-        elif voice == "CUTE":
-            _voiceType = "en-US-AnaNeural"
-            _voiceGender = "Female"
+        # Send the request to Groq
+        async with _client_session.post(_endpoint, headers=_headers, json=_payload) as response:
+            if response.status != 200:
+                raise Exception(f"Error: {response.status} - {await response.text()}")
 
-        _xmlData = inspect.cleandoc(rf"""<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
-                <voice xml:gender='{_voiceGender}' name='{_voiceType}'>
-                    {text}
-                </voice>
-            </speak>""")
+            # Check if mime type is audio/wav
+            if "audio/wav" not in response.headers["Content-Type"]:
+                raise Exception("The response from Groq is not in audio/wav format")
 
-        # Make a request
-        async with _client_session.post(f"https://{environ["AZURE_TTS_REGION"]}.tts.speech.microsoft.com/cognitiveservices/v1", data=_xmlData, headers=_headers) as _response:
-            if _response.status != 200:
-                raise Exception(f"Failed to generate audio with code {_response.status}, reason: {_response.reason}")
-            
-            # Ensure the output is audio and in binary format
-            if not "audio" in _response.headers["Content-Type"]:
-                raise Exception("The response from the Azure TTS API is not in audio format")
+            # Read the audio data
+            _audio = await response.read()
 
-            # Send the audio
-            _audio = await _response.content.read()
-
-        # Send the image
-        await self.method_send(file=discord.File(fp=io.BytesIO(_audio), filename="voice.mp3"))
+        # Send the audio
+        await self.method_send(file=discord.File(fp=io.BytesIO(_audio), filename="voice.wav"))
 
         # Cleanup
         return "Audio success and the file should be sent automatically"
