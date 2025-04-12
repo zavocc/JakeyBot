0f12a9774f0e8e93427c8501bfe755732986d28a
WMCB Tech (marcusz) <zavocc1030@outlook.com>
2025-04-12 08:27:02 +0000

    Update ImageGen description

--- tools/ImageGen/manifest.py
+++ tools/ImageGen/manifest.py
@@ -1,16 +1,17 @@
 class ToolManifest:
     tool_human_name = "Image Generation and Editing"
+    image_generator_tool_description = "Generate or edit an image with Gemini 2.0 Flash's image generation capabilities"
     def __init__(self):
         self.tool_schema = [
             {
                 "name": "image_generator",
-                "description": "Generate or edit an image with Gemini 2.0 Flash's image generation capabilities",
+                "description": self.image_generator_tool_description,
                 "parameters": {
                     "type": "OBJECT",
                     "properties": {
                         "prompt": {
                             "type": "STRING",
-                            "description": "The prompt for Gemini to generate or edit the image, it's recommended to keep it elaborate based on user's intent and add a prompt to generate or edit an image"
+                            "description": "The prompt for Gemini to generate or edit the image."
                         },
                         "temperature": {
                             "type": "INTEGER",
@@ -18,7 +19,7 @@ class ToolManifest:
                         },
                         "discord_attachment_url": {
                             "type": "STRING",
-                            "description": "The Discord attachment URL for image to be referenced or edited"
+                            "description": "The Discord attachment URL for image to be referenced or edited. When editing an image, it's recommended to keep the prompt simple to prevent unnecessary elements being added or deviations as it is a LLM model that is contextually capable at editing images, you do not need to describe the original image as prompt again if the attachment was provided, just prompt the requested changes without adding extra to little info of the image. For example, when a cat image is provided and user asks to edit it to have a hat, just say add a hat."
                         },
                         "text_controls": {
                             "type": "STRING",
@@ -36,21 +37,21 @@ class ToolManifest:
                 "type": "function",
                 "function": {
                     "name": "image_generator",
-                    "description": "Generate or edit an image with Gemini 2.0 Flash's image generation capabilities",
+                    "description": self.image_generator_tool_description,
                     "parameters": {
                         "type": "object",
                         "properties": {
                             "prompt": {
                                 "type": "string",
-                                "description": "The prompt for Gemini to generate or edit the image, it's recommended to keep it elaborate based on user's intent and add a prompt to generate or edit an image"
+                                "description": "The prompt for Gemini to generate or edit the image."
                             },
                             "temperature": {
                                 "type": "integer",
-                                "description": "Parameter for the LLM that controls diversity and variety of the generated content, it's recommended to keep the temp values below 1.2"
+                                "description": "Parameter for the LLM that controls diversity and variety of the generated content, it's recommended to keep the temp values below 1.2."
                             },
                             "discord_attachment_url": {
                                 "type": "string",
-                                "description": "The Discord attachment URL for image to be referenced or edited"
+                                "description": "The Discord attachment URL for image to be referenced or edited. When editing an image, it's recommended to keep the prompt simple to prevent unnecessary elements being added or deviations as it is a LLM model that is contextually capable at editing images, you do not need to describe the original image as prompt again if the attachment was provided, just prompt the requested changes without adding extra to little info of the image. For example, when a cat image is provided and user asks to edit it to have a hat, just say add a hat."
                             },
                             "text_controls": {
                                 "type": "string",
